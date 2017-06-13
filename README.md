7ab9a6062902ae61c0a82cc89b5153ade0863b5e
Ivan Man <ivan@yieldone.com.hk>
2017-06-13 14:26:33 +0800

    add other directives

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # ngInput
+
+# Demo
+[Plunker Link]
+
+# How to install
+```sh
+bower install ngInput
+```
+# How to use
+There are 4 directives:
+- ng-input-text
+- ng-input-number
+- ng-input-select
+- ng-input-date
+
+
+License
+----
+MIT
+
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+
+   [dill]: <https://github.com/joemccann/dillinger>
+   [git-repo-url]: <https://github.com/joemccann/dillinger.git>
+   [john gruber]: <http://daringfireball.net>
+   [df1]: <http://daringfireball.net/projects/markdown/>
+   [markdown-it]: <https://github.com/markdown-it/markdown-it>
+   [Ace Editor]: <http://ace.ajax.org>
+   [node.js]: <http://nodejs.org>
+   [Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
+   [jQuery]: <http://jquery.com>
+   [@tjholowaychuk]: <http://twitter.com/tjholowaychuk>
+   [express]: <http://expressjs.com>
+   [AngularJS]: <http://angularjs.org>
+   [Gulp]: <http://gulpjs.com>
+   [Plunker Link]: <https://plnkr.co/2WQHOo>
+
+   [PlDb]: <https://github.com/joemccann/dillinger/tree/master/plugins/dropbox/README.md>
+   [PlGh]: <https://github.com/joemccann/dillinger/tree/master/plugins/github/README.md>
+   [PlGd]: <https://github.com/joemccann/dillinger/tree/master/plugins/googledrive/README.md>
+   [PlOd]: <https://github.com/joemccann/dillinger/tree/master/plugins/onedrive/README.md>
+   [PlMe]: <https://github.com/joemccann/dillinger/tree/master/plugins/medium/README.md>
+   [PlGa]: <https://github.com/RahulHP/dillinger/blob/master/plugins/googleanalytics/README.md>
--- ngInput.js
+++ ngInput.js
@@ -143,13 +143,175 @@ mod.directive('ngInputText', [ '$q', '$timeout', 'ngInputLocalize', function($q,
   };
 } ]);
 
+
+
+
+
+
+mod.directive('ngInputNumber', ['$q', '$timeout', 'ngInputLocalize', function($q, $timeout, localize) {
+  return {
+    restrict: 'E',
+    scope : {
+      bindModel: "=",
+      min: "=?",
+      max: "=?",
+      step: "=?",
+      pattern: "=?",
+      validators: "=?",
+      asyncValidators: "=?",
+      callBack: "&?"
+    },
+    templateUrl: 'ngInputNumberTemplate.html',
+    
+    controller: function($scope) {
+      
+      $scope.modelOptions =  angular.isDefined($scope.asyncValidators) ? { 'updateOn': 'default blur', 'debounce': {'default':400, 'blur': 0} } : { 'updateOn': 'default blur', 'debounce': {'default':0, 'blur': 0} };
+      $scope.min = angular.isDefined($scope.min) ? $scope.min : 1;
+      $scope.max = angular.isDefined($scope.max) ? $scope.max : 100000000000;
+      $scope.step = angular.isDefined($scope.step) ? $scope.step : 1;
+      $scope.pattern = angular.isDefined($scope.pattern) ? $scope.pattern : "^\\d+(?:\\.\\d{1,2})?$";
+    
+    
+    },            
+    link: function(scope) {
+      
+      scope.inputValidationPopover = {
+          control: {}
+      };
+      
+      scope.initInputValidationPopover = function(control) {
+        scope.inputValidationPopover.control = control;
+      }
+      
+      scope.getLocalizedText = function(key){
+        return localize.localizeText(key);
+      }
+    
+    }
+  };
+}]);
+
+
+mod.directive('ngInputSelect', ['$q', '$timeout', 'ngInputLocalize', function($q, $timeout, localize) {
+  return {
+    restrict: 'E',
+    scope : {
+      bindModel: "=",
+      optionList: "=",
+      groupBy: '@?',
+      orderBy: '@?',
+      validators: "=?",
+      asyncValidators: "=?",
+      showValue: "@?",
+      callBack: "&?"
+    },
+    templateUrl: 'ngInputSelectTemplate.html',
+    
+    controller: function($scope) {
+      
+      $scope.orderBy =  angular.isDefined($scope.orderBy) ? $scope.orderBy : "'value'";
+      $scope.showValue =  angular.isDefined($scope.showValue) ? $scope.showValue : 'true';
+      
+      
+      $scope.options = "i.value as padValueName(i.value, i.name) for i in optionList | orderBy:" + $scope.orderBy;
+      if (angular.isDefined($scope.groupBy)) {
+        $scope.options = "i.value as padValueName(i.value, i.name) group by i." + $scope.groupBy +" for i in optionList | orderBy:" + $scope.orderBy;
+      }
+      
+       
+      
+      $scope.padValueName = function(value, name) {
+        
+        if ($scope.showValue === 'true') {
+          var spaces = " ";
+          for (var i = value.length; i < 7; i++)
+            spaces += String.fromCharCode(160);
+          
+          return value + spaces + name;
+        } else {
+          return name;
+        }
+      }
+    
+    },
+    link: function(scope) {
+      
+      scope.inputValidationPopover = {
+          control: {}
+      };
+      
+      scope.initInputValidationPopover = function(control) {
+        scope.inputValidationPopover.control = control;
+      }
+      
+      scope.getLocalizedText = function(key){
+        return localize.localizeText(key);
+      }
+    
+    }
+  };
+}]);
+
+
+
+
+mod.directive('ngInputDate', ['$q', '$timeout', 'ngInputLocalize', function($q, $timeout, localize) {
+  return {
+    restrict: 'E',
+    
+    scope : {
+      bindModel: "=",
+      validators: "=?",
+      asyncValidators: "=?"
+    },
+    templateUrl: 'ngInputDateTemplate.html',
+    controller: function($scope) {
+      
+    },  
+    link: function(scope, elm, attrs, ctrl) {
+      
+      
+      scope.inputValidationPopover = {
+          control: {}
+      };
+      
+      scope.initInputValidationPopover = function(control) {
+        scope.inputValidationPopover.control = control;
+      }
+      
+      scope.getLocalizedText = function(key){
+        return localize.localizeText(key);
+      }
+    
+    }
+  };
+}]);      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 mod
     .run([
         'ngInputLocalize',
         '$templateCache',
 
         function(ngInputLocalize, $templateCache) {
-          //ngInputLocalize.forDev = true;
           ngInputLocalize.setLang(ngInputLocalize.currentLang);
           
 
@@ -165,17 +327,83 @@ mod
                         </li>\
                       </div>\
                     </div>';
-          $templateCache.put('popoverTemplate.html', popoverTpl);
+          $templateCache.put('ngInputPopoverTemplate.html', popoverTpl);
 
+          
+          //text, email
           var ngInputTextTpl = '<ng-form name="tmpForm"><span class="input-group" ng-class="{\'has-error\': tmpForm.ctrlName.$invalid, \'has-success\': tmpForm.ctrlName.$valid}">\
             <input required class="form-control" type="{{type}}" ng-model="bindModel" name="ctrlName" ng-pattern="pattern" add-validation ng-model-options="modelOptions" \>\
             <span class="input-group-addon" popover-placement="{{popoverPlacement}}" \
-            popover-template="\'popoverTemplate.html\'" \
+            popover-template="\'ngInputPopoverTemplate.html\'" \
             popover-append-to-body={{popoverAppendToBody}} popover-trigger="mouseenter" \
             ng-mouseenter="initInputValidationPopover(tmpForm.ctrlName)"> \
             <i ng-class="{\'glyphicon glyphicon-info-sign\': tmpForm.ctrlName.$invalid, \'glyphicon glyphicon-ok-sign\': tmpForm.ctrlName.$valid, \'fa fa-spin fa-spinner\': tmpForm.ctrlName.$pending}" />\
             </span></span></ng-form>';
 
           $templateCache.put('ngInputTextTemplate.html', ngInputTextTpl);
+          
+          
+          
+          //number
+          var ngInputNumberTpl = '<ng-form name="tmpForm"><span class="input-group" ng-class="{\'has-error\': tmpForm.ctrlName.$invalid, \'has-success\': tmpForm.ctrlName.$valid}">\
+          <input required class="form-control" type="number" ng-model="bindModel" name="ctrlName" min={{min}} max={{max}} step={{step}} ng-pattern="pattern" add-validation ng-model-options="modelOptions" ng-change="callBack()" />\
+          <span class="input-group-addon" popover-placement="right" \
+          popover-template="\'ngInputPopoverTemplate.html\'" \
+          popover-append-to-body="true" popover-trigger="mouseenter" \
+          ng-mouseenter="initInputValidationPopover(tmpForm.ctrlName)">\
+          <i ng-class="{\'glyphicon glyphicon-info-sign\': tmpForm.ctrlName.$invalid, \'glyphicon glyphicon-ok-sign\': tmpForm.ctrlName.$valid, \'fa fa-spin fa-spinner\': tmpForm.ctrlName.$pending}"/>\
+          </span></span></ng-form>';
+          
+          
+          $templateCache.put('ngInputNumberTemplate.html', ngInputNumberTpl);
+          
+          
+          
+          
+          //option select
+          var ngInputSelectTpl = '<ng-form name="tmpForm"><span class="input-group" ng-class="{\'has-error\': tmpForm.ctrlName.$invalid, \'has-success\': tmpForm.ctrlName.$valid}">\
+          <select  required class="form-control" ng-model="bindModel" name="ctrlName" \
+          ng-options="{{options}}" add-validation ng-change="callBack()" />\
+                    <span class="input-group-addon" popover-placement="right" \
+                    popover-template="\'ngInputPopoverTemplate.html\'" \
+                    popover-append-to-body="true" popover-trigger="mouseenter" \
+                    ng-mouseenter="initInputValidationPopover(tmpForm.ctrlName)">\
+                    <i ng-class="{\'glyphicon glyphicon-info-sign\': tmpForm.ctrlName.$invalid, \'glyphicon glyphicon-ok-sign\': tmpForm.ctrlName.$valid, \'fa fa-spin fa-spinner\': tmpForm.ctrlName.$pending}"/>\
+                    </span></span></ng-form>';
+          
+          
+          $templateCache.put('ngInputSelectTemplate.html', ngInputSelectTpl);
+          
+          
+          
+          //Date
+          var ngInputDateTpl = '<ng-form name="tmpForm"><span class="input-group"\
+                  ng-class="{\'has-error\': tmpForm.ctrlName.$invalid, \'has-success\': tmpForm.ctrlName.$valid }">\
+                  <input type="text" class="form-control" datepicker-popup="yyyy-MMM-dd"\
+                    ng-model="bindModel" is-open="holidayDateIsOpen"\
+                    disabled="true" placeholder="yyyy-MMM-dd" name="ctrlName"\
+                    add-validation required />\
+                  <span class="input-group-btn">\
+                    <button type="button" class="btn btn-default"\
+                      ng-click=" $event.stopPropagation(); holidayDateIsOpen=true;">\
+                      <i class="glyphicon glyphicon-calendar"></i>\
+                    </button>\
+                    <button class="btn" popover-placement="right"\
+                    popover-template="\'ngInputPopoverTemplate.html\'"\
+                    popover-append-to-body="true" popover-trigger="mouseenter"\
+                    ng-mouseenter="initInputValidationPopover(tmpForm.ctrlName)"\
+                    ng-class="{\'btn-danger\': tmpForm.ctrlName.$invalid, \'btn-success\': tmpForm.ctrlName.$valid}">\
+                    <i ng-class="{\'glyphicon glyphicon-info-sign\':tmpForm.ctrlName.$invalid, \'glyphicon glyphicon-ok-sign\':tmpForm.ctrlName.$valid, \'fa fa-spin fa-spinner\': tmpForm.ctrlName.$pending}" />\
+                    </button>\
+                    </span></span></ng-form>';
+          
+          
+          $templateCache.put('ngInputDateTemplate.html', ngInputDateTpl);
+          
+          
+          
+          
+          
+          
 
         } ]);
